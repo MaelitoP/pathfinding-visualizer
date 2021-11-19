90013640108eb3cee7840c03f20792e27582d3e2
Maël LE PETIT <makibeardy@gmail.com>
2021-11-19 15:35:20 -0500

    Disabled mobile device view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 import './App.css';
 
 import Navbar from './components/navbar/Navbar';
 import PathfindingVisualizer from './components/PathfindingVisualizer';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <PathfindingVisualizer></PathfindingVisualizer>
+  if (isMobile) {
+    <div className="mobile">
+      <h3>The Pathfinding Visualizer app is unavailable on mobile.</h3>
     </div>
-  );
+  } else {
+    return (
+      <div className="App">
+        <Navbar />
+        <PathfindingVisualizer></PathfindingVisualizer>
+      </div>
+    );
+  }
 }
 
 export default App;
